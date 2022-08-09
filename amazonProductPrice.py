354faef25d3d1455399160e52a94522c72740dd6
raadfortest <raad.elmsellati@pcc.edu>
2022-08-09 15:56:47 -0700

    first commit

---
+++ amazonProductPrice.py
@@ -0,0 +1,53 @@
+#Import needed libraries
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium.webdriver.chrome.options import Options
+
+#Set Chrome for webdriver
+
+optionss = Options()
+optionss.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(executable_path='C:\est\chromedriver.exe', options=optionss)
+#Lists of needed data
+laptops=[]
+prices=[]
+rate=[]
+
+#Website requested..
+driver.get("https://www.amazon.com/s?k=laptop&sprefix=lap%2Caps%2C410&ref=nb_sb_ss_retrain-deeppltr_1_3")
+
+content = driver.page_source
+soup = BeautifulSoup(content,features="html.parser")
+
+for info in soup.findAll('div', attrs={'class':'a-section a-spacing-small a-spacing-top-small'}):
+
+    pName = info.find('span', attrs={'class':'a-size-medium a-color-base a-text-normal'})
+    if pName is not None:
+        laptops.append(pName.text)
+    else:
+        pName = None
+
+    pPrice = info.find('span', attrs={'class':'a-offscreen'})
+    if pPrice is not None:
+        prices.append(pPrice.text)
+    else:
+        pPrice = None
+
+    pRating = info.find('span', attrs={'class': 'a-icon-alt'})
+    if pRating is not None:
+        rate.append(pRating.text)
+    else:
+        pRating = None
+    
+    #prices.append(pPrice.text)
+    #rate.append(pRating.text)
+#Push the products to the lists
+
+
+print("Now creating the cvs file ...")
+df = pd.DataFrame({'Product Name':laptops,'Price':prices,'Rating':rate}) 
+df.to_csv('products.csv', index=False, encoding='utf-8')
+print("Created")
+
